6968bdf750f1f4d8bf08ad0053b3eecbe0e90de4
moohoozaa <paramate.php@gmai.com>
2019-10-05 00:28:42 +0700

    add api key to heroku

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai')
 
 const app = new Clarifai.App({
-  apiKey: '55da84a25c4144b7a3109c3262775d85'
+  apiKey: process.env.API_CLARIFAI
 })
 
 module.exports.handleApiCall = (req, res) => {
